932c1e6477c09712ac882c5936a3365abfbb0f22
jpaobandong <jpaobandong@gmail.com>
2016-06-30 16:11:28 +0800

    signup form with error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  before_save { self.username = username.downcase }
-  validates :username, presence: true, length: { maximum: 20 }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { maximum: 20 }, uniqueness: { case_sensitive: true }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
 
+  resources :users
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
